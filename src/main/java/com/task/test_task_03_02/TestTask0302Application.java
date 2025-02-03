d7cd08408b3d738e04ad378aa284f18393158080
MaxonRash <rashnikov.m@yandex.com>
2025-02-03 17:26:56 +0300

    initial commit

---
+++ src/main/java/com/task/test_task_03_02/TestTask0302Application.java
@@ -0,0 +1,13 @@
+package com.task.test_task_03_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTask0302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTask0302Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/test_task_03_02/TestTask0302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.test_task_03_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTask0302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
